a1082dcbf352ee97a8e42006097ac8819fe60e9f
landaverde1196 <alejndro1196@gmail.com>
2020-10-22 19:52:19 -0600

    restaurant list & AddRestaurant added

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -2,8 +2,8 @@ import React from "react";
 
 const Header = () => (
   <div>
-    <h1 className="font-weight-light display-1 text-center">
-      Restaurant Finder!
+    <h1 className="font-weight-lighter display-1 text-center">
+      Restaurant Finder
     </h1>
   </div>
 );
