d371d0f7d04be4af78a4d12af3e8fd1908992f45
landaverde1196 <alejndro1196@gmail.com>
2020-10-29 19:23:38 -0600

    psql examples

--- server/db/db.sql
+++ server/db/db.sql
@@ -1,7 +1,52 @@
+-- postgresqltutorial.com
+-- Create table POSTGRES
 CREATE TABLE reviews (
     id BIGSERIAL NOT NULL PRIMARY KEY,
     restaurant_id BIGINT NOT NULL REFERENCES restaurants(id),
     name VARCHAR(50) NOT NULL,
     review TEXT NOT NULL,
-    rating INT check(rating >= 1 and rating <=5)
+    rating INT check(
+        rating >= 1
+        and rating <= 5
+    )
 );
+
+-- PSQL aggregate functions  --
+
+-- select as (name)---------------------------------
+select
+    count(rating) as rating_count
+from
+    reviews;
+
+-- 2------------------------------------------------------------------
+select
+    trunc(avg(rating), 2) as ratings
+from
+    reviews
+where
+    restaurant_id = 2;
+
+-- //group by---------------------------------------------------------------
+select
+    name,
+    count(name)
+from
+    reviews
+group by
+    name;
+
+--2-------------------------------------------------------------
+select
+    restaurant_id,
+    avg(rating)
+from
+    reviews
+group by
+    restaurant_id;
+
+-- trunc decimals---------------------------------------------------------
+select
+    trunc(avg(rating), 2) as avg_rating
+from
+    reviews;
